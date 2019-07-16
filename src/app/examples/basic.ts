46e846a14ee90331131a3a176f8cb6a06a821c31
lintojohny <linto.johny@experionglobal.com>
2019-07-16 13:05:43 +0530

    updates to select box in selected fields

--- src/app/examples/basic.ts
+++ src/app/examples/basic.ts
@@ -1,64 +1,93 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild } from '@angular/core';
+import { AngularMultiSelect } from 'projects/angular2-multiselect-dropdown-lib/src/lib';
 
 @Component({
     templateUrl: './views/view.html'
 })
 export class BasicExample implements OnInit {
 
-    itemList = [];
+
+    dropdownList = [];
     selectedItems = [];
-    settings = {};
-    selectAllValue = 10000;
+    dropdownSettings = {};
+  
     constructor() { }
+  
     ngOnInit() {
-
-        this.itemList = [
-            { "countryId": 1, "itemName": "India" },
-            { "countryId": 2, "itemName": "Singapore" },
-            { "countryId": 3, "itemName": "Australia" },
-            { "countryId": 4, "itemName": "Canada" },
-            { "countryId": 5, "itemName": "South Korea" },
-            { "countryId": 6, "itemName": "Brazil" }
-        ];
-
-        this.selectedItems = [
-            { "countryId": 1, "itemName": "India" },
-            { "countryId": 2, "itemName": "Singapore" },
-            { "countryId": 3, "itemName": "Australia" },
-            { "countryId": 4, "itemName": "Canada" },
-            { "countryId": 5, "itemName": "South Korea" },
-            { "countryId": 6, "itemName": "Brazil" }];
-
-        this.settings = {
-            text: "Select Countries",
-            selectAllText: 'Select All',
-            unSelectAllText: 'UnSelect All',
-            classes: "myclass custom-class",
-            primaryKey: "countryId"
-        };
+  
+      this.dropdownList = [
+        {"id":1,"itemName":"India", "reqN": "India", "isSelected": "false" },
+        {"id":2,"itemName":"Singapore", "reqN": "Singapore", "isSelected": "false" },
+        {"id":3,"itemName":"Australia", "reqN": "Australia", "isSelected": "false" },
+        {"id":4,"itemName":"Canada", "reqN": "Canada", "isSelected": "false" },
+        {"id":5,"itemName":"South Korea", "reqN": "Korea", "isSelected": "false" },
+        {"id":6,"itemName":"Germany", "reqN": "Germany", "isSelected": "false" },
+        {"id":7,"itemName":"France", "reqN": "France", "isSelected": "false" },
+        {"id":8,"itemName":"Russia", "reqN": "Russia", "isSelected": "false" },
+        {"id":9,"itemName":"Italy", "reqN": "Italy", "isSelected": "false" },
+        {"id":10,"itemName":"Sweden", "reqN": "Sweden", "isSelected": "false" }
+      ];
+  this.selectedItems = [
+     
+      ];
+  this.dropdownSettings = { 
+            singleSelection: false, 
+            text:"Select Countries",
+            selectAllText:'Select All',
+            unSelectAllText:'UnSelect All',
+            enableSearchFilter: true,
+            classes:"myclass custom-class",
+            showCheckbox: true
+  
+          };            
     }
-    onItemSelect(item: any) {
-        console.log(item);
-        console.log(this.selectedItems);
-    }
-    OnItemDeSelect(item: any) {
-        console.log(item);
-        console.log(this.selectedItems);
-    }
-    onSelectAll(items: any) {
-        console.log(items);
-    }
-    onDeSelectAll(items: any) {
-        console.log(items);
-    }
-    getSelectAllValue($event){
-        console.log('event', $event);
-        
-    }
-    cssgist: boolean = false;
-    title: string = "Basic example";
-    tsgist: string = "CuppaLabs/ee72fbc7b21dad7e4e7664c5b1553235";
-    htmlgist: string = "CuppaLabs/eb78d42ab7971fda6493586e329bfdb8";
-    tstitle: string = "basic.ts"
-    htmltitle: string = "basic.html";
+  
+    onItemSelect(item:any){
+      console.log(item);
+      item.isSelected = true;
+      console.log('dropdownSettings', this.dropdownSettings);
+      console.log(this.dropdownList);
+  }
+  
+  OnItemDeSelect(item:any){
+      item.isSelected = false;
+      console.log(item);
+      console.log('dropdownSettings', this.dropdownSettings);
+  
+      console.log(this.dropdownList);
+  }
+  onSelectAll(items: any){
+      console.log(items);
+  }
+  onDeSelectAll(items: any){
+      console.log(items);
+  }
+  getFocusOut(item, value){
+    console.log('value', value);
+    item.reqN = value;
+    console.log(this.dropdownList);
+    
+  }
+  
+  getFocus(item){
+    console.log('get focus');
+    event.stopPropagation();
+    item.stopPropagation();
+  
+  }
+  
+  onGroupSelect($event){
+    console.log($event);
+    
+  }
+  getSelectAllValue($event){
+    console.log('event', $event);
+    
 }
+  
+  onClickOutideDropdown($event){
+    console.log("event>>>>>>>", $event);
+    // this.varietalDropdown.openDropdown()
+  }
+  }
+  
